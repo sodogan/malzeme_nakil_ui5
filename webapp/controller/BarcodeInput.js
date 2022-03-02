e35c54b209f2a5257d402779a17f9b2ed89896be
sodogan <itelligence.i.dogan@daimler.com>
2022-03-02 13:45:39 +0300

    moved fragments seperate folder

--- webapp/controller/BarcodeInput.js
+++ webapp/controller/BarcodeInput.js
@@ -1,115 +1,116 @@
-sap.ui.define([
-	"com/bozankaya/ZBZ_MALZEME_NAKIL/controller/BaseController",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/ui/model/json/JSONModel"
-], function (BaseController, Filter, FilterOperator, JSONModel) {
-	"use strict";
+sap.ui.define(
+  [
+    "com/bozankaya/ZBZ_MALZEME_NAKIL/controller/BaseController",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (BaseController, Filter, FilterOperator, JSONModel) {
+    "use strict";
 
-	return BaseController.extend("com.bozankaya.ZBZ_MALZEME_NAKIL.controller.BarcodeInput", {
+    return BaseController.extend(
+      "com.bozankaya.ZBZ_MALZEME_NAKIL.controller.BarcodeInput",
+      {
+        _getDialog: function () {
+          if (!this.oBarcodeInput) {
+            this.oBarcodeInput = sap.ui.xmlfragment(
+              "com.bozankaya.ZBZ_MALZEME_NAKIL.view.fragments.BarcodeInput",
+              this
+            );
+          }
+          return this.oBarcodeInput;
+        },
+        open: function (oView, oController) {
+          var oDialog = this._getDialog();
+          this.oView = oView;
+          this.oController = oController;
+          jQuery.sap.syncStyleClass(
+            this.getOwnerComponent().getContentDensityClass(),
+            oView,
+            oDialog
+          );
+          oController.getView().addDependent(oDialog);
+          oDialog.open();
+        },
+        onCloseBarcodeInputDialog: function () {
+          this._getDialog().close();
+        },
+        onDialogAfterClose: function () {
+          this.oBarcodeInput.destroy();
+          this.oBarcodeInput = undefined;
+        },
+        onChangeBarcodeInput: function (oEvent) {
+          var barcode = oEvent.getSource().getValue();
 
-		_getDialog: function () {
-			if (!this.oBarcodeInput) {
-				this.oBarcodeInput = sap.ui.xmlfragment("com.bozankaya.ZBZ_MALZEME_NAKIL.view.BarcodeInput", this);
-			}
-			return this.oBarcodeInput;
-		},
-		open: function (oView, oController) {
-			var oDialog = this._getDialog();
-			this.oView = oView;
-			this.oController = oController;
-			jQuery.sap.syncStyleClass(this.getOwnerComponent().getContentDensityClass(), oView, oDialog);
-			oController.getView().addDependent(oDialog);
-			oDialog.open();
-		},
-		onCloseBarcodeInputDialog: function () {
-			this._getDialog().close();
-		},
-		onDialogAfterClose: function () {
+          // var oBarcodeModel = new JSONModel({
+          // 	barcode: barcode
+          // });
+          // this.getView().setModel(oBarcodeModel, "Barcode");
+          this._getDialog().close();
+          this.onDialogAfterClose();
 
-			this.oBarcodeInput.destroy();
-			this.oBarcodeInput = undefined;
-		},
-		onChangeBarcodeInput: function (oEvent) {
+          // this._clearDialog();
 
-			var barcode = oEvent.getSource().getValue();
+          // var oModel = this.getModelAndSetHeaders();
+          var that = this;
+          // var oModelJson = new sap.ui.model.json.JSONModel();
 
-			// var oBarcodeModel = new JSONModel({
-			// 	barcode: barcode
-			// });
-			// this.getView().setModel(oBarcodeModel, "Barcode");
-			this._getDialog().close();
-			this.onDialogAfterClose();
+          var oFilters = [];
+          var oModel = this.getView().getModel();
 
-			// this._clearDialog();
+          oFilters.push(new Filter("Formno", FilterOperator.EQ, barcode));
+          oModel.read("/DetailSet", {
+            filters: oFilters,
+            success: function (oData, oResponse) {
+              var jsonForm = new sap.ui.model.json.JSONModel();
+              jsonForm.setData(oData);
+              jsonForm.setSizeLimit(999999);
 
-			// var oModel = this.getModelAndSetHeaders();
-			var that = this;
-			// var oModelJson = new sap.ui.model.json.JSONModel();
+              // sap.ui.getCore().setModel(jsonForm, "Item");
+              that.getOwnerComponent().setModel(jsonForm, "Item");
+              that.getOwnerComponent().setModel(jsonForm, "List");
+              var oItem = that.getOwnerComponent().getModel("Item").oData;
 
-			var oFilters = [];
-			var oModel = this.getView().getModel();
+              if (oItem.results.length > 0) {
+                var oDetailHeaderModel = new JSONModel({
+                  Formno: oItem.results[0].Formno,
+                  Lgort: oItem.results[0].Lgort,
+                  Rsnum: oItem.results[0].Rsnum,
+                  LgortK: oItem.results[0].LgortK,
+                  MatnrS: oItem.results[0].MatnrS,
+                });
+                that.getOwnerComponent().setModel(oDetailHeaderModel, "Header");
+                // var oHeader = that.getView().getModel("Header").oData;
 
-			oFilters.push(new Filter("Formno", FilterOperator.EQ, barcode));
-			oModel.read("/DetailSet", {
-				filters: oFilters,
-				success: function (oData, oResponse) {
+                // var oNav = that.getOwnerComponent().getModel("Nav").oData;
+                // oNav.Nav = "1".
+                // that.getOwnerComponent().setModel(oNav, "Nav");
+                var oNavModel = new JSONModel({
+                  Nav: "1",
+                });
+                that.getOwnerComponent().setModel(oNavModel, "Nav");
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
 
-					var jsonForm = new sap.ui.model.json.JSONModel();
-					jsonForm.setData(oData);
-					jsonForm.setSizeLimit(999999);
+                oRouter.navTo("Detail");
+              }
+            },
+            error: function (oData, oResponse) {
+              //execute in case of call fail
+              var obj = JSON.parse(oData.response.body);
+              var msg = obj.error.message.value;
+              sap.m.MessageBox.show(msg, {
+                icon: sap.m.MessageBox.Icon.ERROR,
+                title: "Error",
+                actions: [sap.m.MessageBox.Action.OK],
+              });
+            },
+          });
+        },
 
-					// sap.ui.getCore().setModel(jsonForm, "Item");
-					that.getOwnerComponent().setModel(jsonForm, "Item");
-					that.getOwnerComponent().setModel(jsonForm, "List");
-					var oItem = that.getOwnerComponent().getModel("Item").oData;
+        _clearDialog: function () {},
 
-					if (oItem.results.length > 0) {
-						var oDetailHeaderModel = new JSONModel({
-							Formno: oItem.results[0].Formno,
-							Lgort: oItem.results[0].Lgort,
-							Rsnum: oItem.results[0].Rsnum,
-							LgortK: oItem.results[0].LgortK,
-							MatnrS: oItem.results[0].MatnrS
-						});
-						that.getOwnerComponent().setModel(oDetailHeaderModel, "Header");
-						// var oHeader = that.getView().getModel("Header").oData;
-						
-						// var oNav = that.getOwnerComponent().getModel("Nav").oData;
-						// oNav.Nav = "1".
-						// that.getOwnerComponent().setModel(oNav, "Nav");
-						var oNavModel = new JSONModel({
-							Nav: "1"
-						});
-						that.getOwnerComponent().setModel(oNavModel, "Nav");
-						var oRouter = sap.ui.core.UIComponent.getRouterFor(that);
-
-						oRouter.navTo("Detail");
-					}
-
-				},
-				error: function (oData, oResponse) {
-					//execute in case of call fail
-					var obj = JSON.parse(oData.response.body);
-					var msg = obj.error.message.value;
-					sap.m.MessageBox.show(msg, {
-						icon: sap.m.MessageBox.Icon.ERROR,
-						title: "Error",
-						actions: [sap.m.MessageBox.Action.OK]
-					});
-				}
-			});
-
-		},
-
-		_clearDialog: function () {
-
-		},
-
-		onSelectShowBarcodeInput: function (oEvent) {
-
-		}
-
-	});
-
-});
+        onSelectShowBarcodeInput: function (oEvent) {},
+      }
+    );
+  }
+);
--- webapp/controller/MessageDialog.js
+++ webapp/controller/MessageDialog.js
@@ -1,47 +1,53 @@
-sap.ui.define([
-	"com/bozankaya/ZBZ_MALZEME_NAKIL/controller/BaseController",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator",
-	"sap/ui/model/json/JSONModel"
-], function (BaseController, Filter, FilterOperator, JSONModel) {
-	"use strict";
+sap.ui.define(
+  [
+    "com/bozankaya/ZBZ_MALZEME_NAKIL/controller/BaseController",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (BaseController, Filter, FilterOperator, JSONModel) {
+    "use strict";
 
-	return BaseController.extend("com.bozankaya.ZBZ_MALZEME_NAKIL.controller.MessageDialog", {
+    return BaseController.extend(
+      "com.bozankaya.ZBZ_MALZEME_NAKIL.controller.MessageDialog",
+      {
+        _getDialog: function () {
+          if (!this.MessageDialog) {
+            this.MessageDialog = sap.ui.xmlfragment(
+              this.oView.getId(),
+              "com.bozankaya.ZBZ_MALZEME_NAKIL.view.fragments.MessageDialog",
+              this
+            );
+          }
+          return this.MessageDialog;
+        },
 
-		_getDialog: function () {
-			if (!this.MessageDialog) {
-				this.MessageDialog = sap.ui.xmlfragment(this.oView.getId(), "com.bozankaya.ZBZ_MALZEME_NAKIL.view.MessageDialog",
-					this);
-			}
-			return this.MessageDialog;
-		},
+        open: function (oController, aMockMessages) {
+          this.oView = oController.getView();
+          this.oController = oController;
 
-		open: function (oController, aMockMessages) {
-			this.oView = oController.getView();
-			this.oController = oController;
+          var oTempModel = new JSONModel();
+          // var iLength = aMockMessages.length - 1;
+          // aMockMessages.splice(iLength, 1);
+          oTempModel.setData(aMockMessages);
 
-			var oTempModel = new JSONModel();
-			// var iLength = aMockMessages.length - 1;
-			// aMockMessages.splice(iLength, 1);
-			oTempModel.setData(aMockMessages);
+          var oDialog = this._getDialog();
 
-			var oDialog = this._getDialog();
+          oDialog.setModel(oTempModel, "message");
+          oController.getView().addDependent(oDialog);
 
-			oDialog.setModel(oTempModel, "message");
-			oController.getView().addDependent(oDialog);
+          oDialog.open();
+        },
 
-			oDialog.open();
-		},
+        onCloseDialog: function () {
+          this._getDialog().close();
+        },
 
-		onCloseDialog: function () {
-			this._getDialog().close();
-		},
-
-		onDialogAfterClose: function () {
-			this.MessageDialog.destroy();
-			this.MessageDialog = undefined;
-		}
-
-	});
-
-});
+        onDialogAfterClose: function () {
+          this.MessageDialog.destroy();
+          this.MessageDialog = undefined;
+        },
+      }
+    );
+  }
+);
